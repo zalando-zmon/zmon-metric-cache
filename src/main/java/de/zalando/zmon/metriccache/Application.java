bacafee90decb74ca04f92cc74efd3aab4635d6a
Alex Korotkykh <alex.korotkykh@zalando.de>
2018-11-21 16:44:12 +0100

    added basic tracing

--- src/main/java/de/zalando/zmon/metriccache/Application.java
+++ src/main/java/de/zalando/zmon/metriccache/Application.java
@@ -1,10 +1,15 @@
 package de.zalando.zmon.metriccache;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import de.zalando.zmon.metriccache.restmetrics.AppMetricsService;
+import de.zalando.zmon.metriccache.restmetrics.VersionResult;
+import io.opentracing.ActiveSpan;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.spring.web.client.HttpHeadersCarrier;
+import io.opentracing.propagation.Format;
+import io.opentracing.tag.Tags;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
@@ -17,6 +22,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -49,122 +55,169 @@ public class Application {
 
     private final Executor executor;
 
-    private static ObjectMapper valueMapper;
-    static {
-        valueMapper = new ObjectMapper();
-        valueMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        valueMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-    }
-
+    private final Tracer tracer;
 
     @Autowired
     public Application(MetricCacheConfig config,
                        ObjectMapper mapper,
                        ApplicationMetricsWriter metricsWriter,
                        AppMetricsService applicationRestMetrics,
-                       Executor executor) {
+                       Executor executor,
+                       Tracer tracer) {
         this.config = config;
         this.mapper = mapper;
         this.metricsWriter = metricsWriter;
         this.applicationRestMetrics = applicationRestMetrics;
         this.executor = executor;
-    }
-
-
-    @ResponseBody
-    @RequestMapping(value="/api/v1/rest-api-metrics/", method=RequestMethod.POST)
-    public void putRestAPIMetrics(@RequestBody String data) throws IOException {
-        // assume for now, that we only receive the right application data
-        List<CheckData> results = mapper.readValue(data, new TypeReference<List<CheckData>>(){});
-        applicationRestMetrics.storeData(results);
+        this.tracer = tracer;
     }
 
     @ResponseBody
-    @RequestMapping(value="/api/v1/rest-api-metrics/unpartitioned", method=RequestMethod.POST)
-    public void putRestAPIMetricsUnpartitioned(@RequestBody String data) throws IOException {
-        // Post data but repartition accross set of hosts (this is already done in data service)
-        List<CheckData> results = mapper.readValue(data, new TypeReference<List<CheckData>>(){});
-        metricsWriter.write(results);
+    @RequestMapping(value = "/api/v1/rest-api-metrics/", method = RequestMethod.POST)
+    public void putRestAPIMetrics(@RequestHeader HttpHeaders headers,
+                                  @RequestBody String data) throws IOException {
+        try (ActiveSpan ignored = createSpan("add_metrics", headers)) {
+            // assume for now, that we only receive the right application data
+            List<CheckData> results = mapper.readValue(data, new TypeReference<List<CheckData>>() {
+            });
+            applicationRestMetrics.storeData(results);
+        }
     }
 
     @ResponseBody
-    @RequestMapping(value="/api/v1/rest-api-metrics/applications", method=RequestMethod.GET)
-    public Collection<String> getRegisteredApplications(@RequestParam(value="global", defaultValue="false") boolean global) throws IOException {
-        // assume for now, that we only receive the right application data
-        return applicationRestMetrics.getRegisteredAppVersions();
+    @RequestMapping(value = "/api/v1/rest-api-metrics/unpartitioned", method = RequestMethod.POST)
+    public void putRestAPIMetricsUnpartitioned(@RequestHeader HttpHeaders headers,
+                                               @RequestBody String data) throws IOException {
+        try (ActiveSpan ignored = createSpan("add_metrics_unpartitioned", headers)) {
+            // Post data but repartition accross set of hosts (this is already done in data service)
+            List<CheckData> results = mapper.readValue(data, new TypeReference<List<CheckData>>() {
+            });
+            metricsWriter.write(results);
+        }
     }
 
     @ResponseBody
-    @RequestMapping(value="/api/v1/rest-api-metrics/tracked-endpoints", method=RequestMethod.GET)
-    public Collection<String> getRegisteredApplications(@RequestParam(value="application_id") String applicationId, @RequestParam(value="global", defaultValue="false") boolean global) throws IOException {
-        // assume for now, that we only receive the right application data
-        return applicationRestMetrics.getRegisteredEndpoints(applicationId);
+    @RequestMapping(value = "/api/v1/rest-api-metrics/applications", method = RequestMethod.GET)
+    public Collection<String> getRegisteredApplications(@RequestHeader HttpHeaders headers,
+                                                        @RequestParam(value = "global", defaultValue = "false") boolean global) {
+        try (ActiveSpan ignored = createSpan("get_registered_applications", headers)) {
+            // assume for now, that we only receive the right application data
+            return applicationRestMetrics.getRegisteredAppVersions();
+        }
     }
 
     @ResponseBody
-    @RequestMapping(value="/api/v1/rest-api-metrics/kairosdb-format", method=RequestMethod.GET)
-    public void getMetricsInKairosDBFormat(Writer writer, HttpServletResponse response, @RequestParam(value="application_id") String applicationId, @RequestParam(value="application_version", defaultValue="1") String applicationVersion, @RequestParam(value="redirect", defaultValue="true") boolean redirect) throws URISyntaxException, IOException {
-        if(config.getRest_metric_hosts().size()==1 && config.getRest_metric_hosts().get(0).equals("localhost")) {
+    @RequestMapping(value = "/api/v1/rest-api-metrics/tracked-endpoints", method = RequestMethod.GET)
+    public Collection<String> getTrackedEndpoints(@RequestHeader HttpHeaders headers,
+                                                  @RequestParam(value = "application_id") String applicationId,
+                                                  @RequestParam(value = "global", defaultValue = "false") boolean global) {
+        try (ActiveSpan ignored = createSpan("get_tracked_endpoints", headers)) {
+            // assume for now, that we only receive the right application data
+            return applicationRestMetrics.getRegisteredEndpoints(applicationId);
+        }
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/v1/rest-api-metrics/kairosdb-format", method = RequestMethod.GET)
+    public void getMetricsInKairosDBFormat(Writer writer,
+                                           HttpServletResponse response,
+                                           @RequestHeader HttpHeaders headers,
+                                           @RequestParam(value = "application_id") String applicationId,
+                                           @RequestParam(value = "application_version", defaultValue = "1") String applicationVersion,
+                                           @RequestParam(value = "redirect", defaultValue = "true") boolean redirect)
+            throws URISyntaxException, IOException {
+        if (config.getRest_metric_hosts().isEmpty() ||
+                (config.getRest_metric_hosts().size() == 1 && config.getRest_metric_hosts().get(0).equals("localhost"))) {
             redirect = false;
         }
 
-        if(!redirect) {
-            try {
+        if (!redirect) {
+            try (ActiveSpan ignored = createSpan("get_metrics_kairosdb_format", headers)) {
                 response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-                writer.write(mapper.writeValueAsString(applicationRestMetrics.getKairosResult(applicationId, applicationVersion, System.currentTimeMillis())));
+                AppMetricsService.KairosDBResultWrapper kairosResult =
+                        applicationRestMetrics.getKairosResult(applicationId, applicationVersion, System.currentTimeMillis());
+                writer.write(mapper.writeValueAsString(kairosResult));
             } catch (IOException ex) {
                 LOG.error("Failed to write metric result to output stream", ex);
             }
-        }
-        else {
-            int hostId = Math.abs(applicationId.hashCode() % config.getRest_metric_hosts().size());
-            String targetHost = config.getRest_metric_hosts().get(hostId);
-            // TODO: we would not need to redirect if the host list is empty or contains only one item (ourself)
-            LOG.info("Redirecting KairosDB metrics request to {} = {}/{}", applicationId, hostId, targetHost);
+        } else {
+            try (ActiveSpan ignored = createSpan("get_metrics_kairosdb_format_redirect", headers)) {
+                int hostId = Math.abs(applicationId.hashCode() % config.getRest_metric_hosts().size());
+                String targetHost = config.getRest_metric_hosts().get(hostId);
+                LOG.info("Redirecting KairosDB metrics request to {} = {}/{}", applicationId, hostId, targetHost);
 
-            URIBuilder builder = new URIBuilder();
-            URI uri = builder.setScheme("http").setHost(targetHost).setPort(Integer.parseInt(config.getServer_port())).setPath("/api/v1/rest-api-metrics/kairosdb-format").setParameter("redirect", "false")
-                    .setParameter("application_id", applicationId)
-                    .setParameter("application_version", applicationVersion).build();
-
-            String body = executor.execute(Request.Get(uri)).returnContent().asString();
-            response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-            writer.write(body);
+                makeRedirect(writer, response, applicationId, applicationVersion, targetHost,
+                        "/api/v1/rest-api-metrics/kairosdb-format");
+            }
         }
     }
 
     @ResponseBody
-    @RequestMapping(value="/api/v1/rest-api-metrics/", method=RequestMethod.GET)
-    public void getRestApiMetrics(Writer writer, HttpServletResponse response, @RequestParam(value="application_id") String applicationId, @RequestParam(value="application_version") String applicationVersion, @RequestParam(value="redirect", defaultValue="true") boolean redirect) throws URISyntaxException, IOException {
-        if(config.getRest_metric_hosts().size()==1 && config.getRest_metric_hosts().get(0).equals("localhost")) {
+    @RequestMapping(value = "/api/v1/rest-api-metrics/", method = RequestMethod.GET)
+    public void getRestApiMetrics(Writer writer,
+                                  HttpServletResponse response,
+                                  @RequestHeader HttpHeaders headers,
+                                  @RequestParam(value = "application_id") String applicationId,
+                                  @RequestParam(value = "application_version") String applicationVersion,
+                                  @RequestParam(value = "redirect", defaultValue = "true") boolean redirect)
+            throws URISyntaxException, IOException {
+        if (config.getRest_metric_hosts().isEmpty() ||
+                (config.getRest_metric_hosts().size() == 1 && config.getRest_metric_hosts().get(0).equals("localhost"))) {
             redirect = false;
         }
 
-        if(!redirect) {
-            try {
+        if (!redirect) {
+            try (ActiveSpan ignored = createSpan("get_metrics", headers)) {
                 response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-                writer.write(mapper.writeValueAsString(applicationRestMetrics.getAggrMetrics(applicationId, applicationVersion, System.currentTimeMillis())));
+                VersionResult metrics =
+                        applicationRestMetrics.getAggrMetrics(applicationId, applicationVersion, System.currentTimeMillis());
+                writer.write(mapper.writeValueAsString(metrics));
             } catch (IOException ex) {
                 LOG.error("Failed to write metric result to output stream", ex);
             }
-        }
-        else {
-            int hostId = Math.abs(applicationId.hashCode() % config.getRest_metric_hosts().size());
-            String targetHost = config.getRest_metric_hosts().get(hostId);
-            LOG.info("Redirecting metrics request to {} = {}/{}", applicationId, hostId, targetHost);
+        } else {
+            try (ActiveSpan ignored = createSpan("get_metrics_redirect", headers)) {
+                int hostId = Math.abs(applicationId.hashCode() % config.getRest_metric_hosts().size());
+                String targetHost = config.getRest_metric_hosts().get(hostId);
+                LOG.info("Redirecting metrics request to {} = {}/{}", applicationId, hostId, targetHost);
 
-            URIBuilder builder = new URIBuilder();
-            URI uri = builder.setScheme("http").setHost(targetHost).setPort(Integer.parseInt(config.getServer_port())).setPath("/api/v1/rest-api-metrics/").setParameter("redirect", "false")
-                                                                                      .setParameter("application_id", applicationId)
-                                                                                      .setParameter("application_version", applicationVersion).build();
-
-            String body = executor.execute(Request.Get(uri)).returnContent().asString();
-            response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-            writer.write(body);
+                makeRedirect(writer, response, applicationId, applicationVersion, targetHost,
+                        "/api/v1/rest-api-metrics/");
+            }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    private void makeRedirect(Writer writer, HttpServletResponse response,
+                              String applicationId, String applicationVersion, String targetHost, String path)
+            throws URISyntaxException, IOException {
+        URIBuilder builder = new URIBuilder();
+        URI uri = builder.setScheme("http")
+                .setHost(targetHost)
+                .setPort(Integer.parseInt(config.getServer_port()))
+                .setPath(path)
+                .setParameter("redirect", "false")
+                .setParameter("application_id", applicationId)
+                .setParameter("application_version", applicationVersion).build();
+
+        String body = executor.execute(Request.Get(uri)).returnContent().asString();
+        response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        writer.write(body);
+    }
+
+
+    private ActiveSpan createSpan(String spanName, HttpHeaders httpHeaders) {
+        final HttpHeadersCarrier carrier = new HttpHeadersCarrier(httpHeaders);
+        final Tracer.SpanBuilder spanBuild = tracer.buildSpan(spanName).withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_SERVER);
+
+        final SpanContext spanContext = tracer.extract(Format.Builtin.HTTP_HEADERS, carrier);
+        if (spanContext != null) {
+            spanBuild.asChildOf(spanContext);
+        }
+
+        return spanBuild.startActive();
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }

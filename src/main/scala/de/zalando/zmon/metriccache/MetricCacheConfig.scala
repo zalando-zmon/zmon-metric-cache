85bd84523cc16df3cd209ee6880655273c08574f
Alex Korotkykh <alex.korotkykh@zalando.de>
2018-11-28 10:28:46 +0100

    remote tracing handler interceptors

--- src/main/scala/de/zalando/zmon/metriccache/MetricCacheConfig.scala
+++ src/main/scala/de/zalando/zmon/metriccache/MetricCacheConfig.scala
@@ -1,10 +1,7 @@
 package de.zalando.zmon.metriccache
 
 import java.util
-import java.util.{Collections, List}
 
-import io.opentracing.contrib.spring.web.interceptor.HandlerInterceptorSpanDecorator
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.{Bean, Configuration}
@@ -14,23 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import scala.beans.BeanProperty
 
 @Component
-@MetricCacheConfig
+@Configuration
 @ConfigurationProperties(prefix = "metriccache")
 class MetricCacheConfig {
 
-  @BeanProperty var rest_metric_hosts : java.util.List[String] = new util.ArrayList[String]()
+  @BeanProperty var rest_metric_hosts: java.util.List[String] = new util.ArrayList[String]()
 
   @Value("${server.port}")
-  @BeanProperty var server_port : String = _
+  @BeanProperty var server_port: String = _
 
-  @Bean def interceptorSpanDecorator(): ObjectProvider[util.List[HandlerInterceptorSpanDecorator]] =
-    new ObjectProvider[util.List[HandlerInterceptorSpanDecorator]] {
-    override def getObject(args: Any*): util.List[HandlerInterceptorSpanDecorator] = getObject
-
-    override def getIfAvailable: util.List[HandlerInterceptorSpanDecorator] = getObject
-
-    override def getIfUnique: util.List[HandlerInterceptorSpanDecorator] = getObject
-
-    override def getObject: util.List[HandlerInterceptorSpanDecorator] = Collections.emptyList()
-  }
+  @Bean def tracingHandlerInterceptor(): WebMvcConfigurerAdapter = new WebMvcConfigurerAdapter() {}
 }

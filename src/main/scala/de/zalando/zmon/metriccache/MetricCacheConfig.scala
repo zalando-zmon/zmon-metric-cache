3ab73995dffce9d63a040bfe18e9c3faa8a699a9
Alex Korotkykh <alex.korotkykh@zalando.de>
2018-11-28 09:57:52 +0100

    remote tracing handler interceptors

--- src/main/scala/de/zalando/zmon/metriccache/MetricCacheConfig.scala
+++ src/main/scala/de/zalando/zmon/metriccache/MetricCacheConfig.scala
@@ -4,18 +4,21 @@ import java.util
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import scala.beans.BeanProperty
 
 @Component
-@Configuration
+@MetricCacheConfig
 @ConfigurationProperties(prefix = "metriccache")
 class MetricCacheConfig {
 
   @BeanProperty var rest_metric_hosts : java.util.List[String] = new util.ArrayList[String]()
 
   @Value("${server.port}")
-  @BeanProperty var server_port : String = null
+  @BeanProperty var server_port : String = _
+
+  @Bean def tracingHandlerInterceptor(): WebMvcConfigurerAdapter = new WebMvcConfigurerAdapter() {}
 }
